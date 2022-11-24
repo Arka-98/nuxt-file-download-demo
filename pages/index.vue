d0b4b104bc0c494275b205dc620e603661402552
Arkadipta <dasarkadipta@gmail.com>
2022-11-24 12:06:01 +0530

    added change pic button

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
         <h1>Hey there!</h1>
         <EmitExample @inline-click="handleInline" />
         <img :src="imgPath" />
+        <button @click="handleClick">Change Pic</button>
     </div>
 </template>
 
@@ -10,6 +11,9 @@
 const imgPath = ref('/_nuxt/assets/img/nuxt3.png')
 const handleInline = () => console.log('inline emit called')
 
+const handleClick = async () => {
+    await $fetch('/api/get-img')
+}
 </script>
 
 <style scoped></style>
